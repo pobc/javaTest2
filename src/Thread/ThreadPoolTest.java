36f1ae86a0f48519ce18e4d39690220f67f5d61f
jiangwj <jiangwj@szcxqj.cn>
2017-09-15 20:20:34 +0800

    FutureTest

--- src/Thread/ThreadPoolTest.java
+++ src/Thread/ThreadPoolTest.java
@@ -5,45 +5,45 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 /**
- * Created by jiang on 2017/9/5.
+ * Created by jiang on 2017/9/11.
  */
-public class FutureTest {
-    public static void main(String args[]) {
+public class ThreadPoolTest {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter base directory(e.g. /usr/local/jdk5.0/src):");
         String directory = in.nextLine();
         System.out.println("Enter keyword (e.g. volatile):");
         String keyword = in.nextLine();
 
-        MatchCounter counter = new MatchCounter(new File(directory), keyword);
-        FutureTask<Integer> task = new FutureTask<Integer>(counter);
-        Thread t = new Thread(task);
-        t.start();
+        ExecutorService pool = Executors.newCachedThreadPool();
+        MatchCounter2 counter = new MatchCounter2(new File(directory), keyword, pool);
+        Future<Integer> result = pool.submit(counter);
         try {
-            System.out.println(task.get() + " matching files");
+            System.out.println(result.get() + " files matched.");
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
+        pool.shutdown();
+        int maxLargestPoolSize = ((ThreadPoolExecutor) pool).getLargestPoolSize();
+        System.out.println("largest pool size=" + maxLargestPoolSize);
 
-        //System.out.println(arrayList.get());
     }
 }
 
-class MatchCounter implements Callable<Integer> {
+class MatchCounter2 implements Callable<Integer> {
 
     private File directory;
     private String keyword;
+    private ExecutorService pool;
     private int count;
 
-    public MatchCounter(File directory, String keyword) {
+    public MatchCounter2(File directory, String keyword, ExecutorService pool) {
         this.directory = directory;
         this.keyword = keyword;
+        this.pool = pool;
     }
 
     @Override
@@ -55,12 +55,10 @@ class MatchCounter implements Callable<Integer> {
 
             for (File file : files) {
                 if (file.isDirectory()) {
-                    MatchCounter counter = new MatchCounter(file, keyword);
-                    FutureTask<Integer> task = new FutureTask<Integer>(counter);
-                    results.add(task);
-                    Thread t = new Thread(task);
-                    //task 将在计算完成之后返回结果
-                    t.start();
+                    MatchCounter2 counter = new MatchCounter2(file, keyword,pool);
+                    Future<Integer> result = pool.submit(counter);
+                    results.add(result);
+
                 } else {
                     if (search(file)) count++;
                 }
